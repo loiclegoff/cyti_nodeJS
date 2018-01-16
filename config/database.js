427920cc6f0bb566591cadb082fe976c39dc850a
Unknown <maguet.antoine@hotmail.fr>
2018-01-16 12:03:14 +0100

    working commit

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@
  */
 
 module.exports = {
-    url : 'mongodb://cyti:20n60sIXXt@195.154.107.158:27017/cyti'
+    //url : 'mongodb://cyti:20n60sIXXt@195.154.107.158:27017/cyti'
+    url : 'mongodb://localhost/test'
     //url : 'mongodb://localhost:27017/cyti'
 };
 
